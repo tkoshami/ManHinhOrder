56db6739d985217469279ee121850252b3b7ac49
Tran Dang Duy <trandangduy13@gmail.com>
2026-06-25 10:53:42 +0700

    feature: add profile screen

--- app_pos-main/lib/main.dart
+++ app_pos-main/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'POS F&B',
+      title: 'MarPOS',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
         useMaterial3: true,
